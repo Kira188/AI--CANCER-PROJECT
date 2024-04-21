fb8423ca5ebb6a11492c093a5f7849ce46ad2599
venkat357 <bhaskarv357@gmail.com>
2024-04-21 14:09:44 +0530

    first commit

---
+++ index02.js
@@ -0,0 +1,39 @@
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+
+const app=express();
+const port=3000;
+var password="";
+var username="";
+const __dirname=dirname(fileURLToPath(import.meta.url));
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname+"/public/index01.html");
+});
+
+function confirmation(req,res,next){
+    password=req.body["password"];
+    console.log(password);
+    next();
+}
+app.use(confirmation);
+
+app.post("/check",(req,res)=>{
+    if(password === "JSSSTU")
+    {
+        res.sendFile(__dirname+"/public/index02.html");
+    }
+    else
+    {
+        res.sendFile(__dirname+"/public/index03.html");
+    }
+});
+
+app.listen(port,()=>{
+    console.log(`the server is listening at port ${port}`);
+});
+
---
+++ public/index.js
@@ -0,0 +1,3 @@
+$("home").click(function(){
+    location.href="http://127.0.0.1:3000/public/index.html";
+});
