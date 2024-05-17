672287757d6465f7acfbe6339c2f49e68612d90d
Kira476 <Darkhorizon476@gmail.com>
2024-05-17 20:13:33 +0530

    Added New Carousel, Jquery cahnges, Background Changes

--- public/index.js
+++ public/index.js
@@ -1,11 +1,23 @@
-document.getElementById("submit-button").addEventListener("mouseover",handleMouseOver);
-function handleMouseOver()
-{
-    document.getElementById("submit-button").style.backgroundColor="gray";
-}
-document.getElementById("submit-button").addEventListener("mouseout",handleMouseOut);
-function handleMouseOut()
-{
-    document.getElementById("submit-button").style.backgroundColor="pink";
-}
+$("#submit-button").on("mouseover",function(){
+    $("#submit-button").css("backgroundColor","gray");
+});
 
+$("#submit-button").on("mouseout",function(){
+    $("#submit-button").css("backgroundColor","pink");
+});
+
+var images = ["./images/carousel-1.jpg","./images/carousel-2.jpg","./images/carousel-3.png" ]
+var currentIndex = 0;
+$(".butt").on("click", function() {
+    currentIndex = (currentIndex + 1) % images.length+1;
+    $(".nimg").fadeOut(function() {
+        $(this).attr("src", images[currentIndex-1]).fadeIn();
+    });
+});
+
+       
+
+
+
+    
+    
